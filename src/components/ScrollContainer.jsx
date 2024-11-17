a2caa88994a67770e8f101a50b1c1e64a744628f
Nishit Lugun <lugun.nishit@gmail.com>
2024-11-18 00:00:43 +0530

    commit

--- src/components/ScrollContainer.jsx
+++ src/components/ScrollContainer.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, Scroll, ScrollControls } from "@react-three/drei";
+import { Scroll, ScrollControls } from "@react-three/drei";
 import React from "react";
 import MainScene from "./MainScene";
 import Page1 from "./Page1";
@@ -6,7 +6,6 @@ import Page1 from "./Page1";
 const ScrollContainer = () => {
   return (
     <>
-      <OrbitControls enableZoom={false} />
       <ScrollControls pages={7} damping={0.8}>
         <MainScene />
         <Scroll html>
