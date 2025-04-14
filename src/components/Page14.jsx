c5d584a1af76d88d9da305551702572b2cb34568
Nishit Lugun <lugun.nishit@gmail.com>
2025-04-15 02:45:56 +0530

    some changes

--- src/components/Page14.jsx
+++ src/components/Page14.jsx
@@ -4,55 +4,55 @@ import React from "react";
 const Page14 = () => {
   const projName = [
     {
-      name: "3D Multiplayer Racing Game",
-      tech: "React, Three fiber, Rapier, Socket.io ",
+      name: "Multiplayer Racing Game",
+
       link: "https://multiplayercarracing-production.up.railway.app/",
     },
     {
-      name: "3D TIC TAC TOE",
-      tech: "React, Three fiber",
-      link: "https://tictactoegame-jxm3.vercel.app/",
-    },
-    {
-      name: "MOBILE DEATHMATCH",
-      tech: "React, Three fiber, Rapier",
+      name: "Mobile Deathmatch Game",
+
       link: "https://multiplayer-shooting-roan.vercel.app/",
     },
     {
+      name: "3d Tic Tac Toe",
+
+      link: "https://tictactoegame-jxm3.vercel.app/",
+    },
+    {
       name: "Gaming Website",
-      tech: "React, GSAP, Tailwind",
+
       link: "https://arcane-gaming-site-jlrz.vercel.app/",
     },
     {
       name: "Toy Store",
-      tech: "Nextjs, GSAP, THREE",
+
       link: "https://toystore-two.vercel.app/",
     },
-    
   ];
   return (
     <>
-      <div className="w-full  sm:h-[100vh] flex items-center flex-col  ">
-        <div className="w-full  text-white sm:mt-[35vw] text-outline-black mt-[25vw] sm:-mb-[0.5vw] mb-[7vw] text-[10vw] font-jelly sm:text-[5.5vw] text-center">
+      <div className="w-full h-fit  sm:h-[100vh] flex items-center flex-col   ">
+        <div className="w-full  text-yellow-500 sm:mt-[35vw] text-outline-black mt-[25vw] sm:-mb-[0.5vw] mb-[7vw] text-[10vw] font-jelly sm:text-[5.5vw] text-center">
           My Projects
         </div>
 
         {projName.map((proj, i) => {
           return (
-            <Link href={proj.link} className="w-full h-full sm:h-fit  z-10">
-              <div className="px-3 w-[100%]">
+            <Link
+              href={proj.link}
+              className="w-full h-fit sm:h-fit  bg-slate-400/35 sm:bg-transparent  z-10"
+            >
+              <div className="px-3 pt-4 sm:px-3 sm:pt-0 w-[100%] sm:w-[70%] sm:mx-auto">
                 <div
                   key={i}
-                  className="project relative font-choco text-black sm:bg-white/50 mb-[5vw] sm:-mb-[0.8vw] bg-white/80 sm:mt-[3.4vw] flex items-center sm:w-[70%]  w-full overflow-hidden mx-auto justify-between sm:px-20 px-6 rounded-full group transition-transform duration-500 ease-out sm:hover:scale-110 hover:scale-105"
+                  className="project relative  text-black sm:bg-white/70 mb-[5vw] sm:-mb-[0.8vw] bg-white/80 sm:mt-[3.4vw] flex items-center sm:w-[70%]  w-full overflow-hidden mx-auto justify-between sm:px-20 px-6 rounded-full group transition-transform duration-500 ease-out sm:hover:scale-110 hover:scale-105"
                 >
-                  <div className="overlay absolute -left-[150%] top-0 sm:w-[200vw] w-[380vw] h-full sm:bg-white/80 bg-white/50 transform sm:-rotate-45 -rotate-90 transition-transform duration-500 ease-in-out sm:group-hover:rotate-0 group-hover:rotate-0"></div>
-                  <h1 className="sm:text-[2.1vw] text-[4vw] z-10">
+                  <div className="overlay absolute -left-[150%] top-0 sm:w-[200vw] w-[380vw] hidden sm:block h-full sm:bg-white/80 bg-white/50 transform sm:-rotate-45 -rotate-90 transition-transform duration-500 ease-in-out sm:group-hover:rotate-0 group-hover:rotate-0"></div>
+                  <h1 className="sm:text-[1.6vw] text-[4vw] z-10">
                     {proj.name}
                   </h1>
-                  <h1 className="sm:text-[1.7vw] text-[3.5vw] absolute sm:text-center sm:left-[0] left-[37%] w-full z-10">
-                    {proj.tech} used
-                  </h1>
-                  <h1 className="sm:text-[1.8vw] text-[3.5vw] text-black z-10">
+
+                  <h1 className="sm:text-[1.4vw] text-[3.3vw] text-black z-10">
                     Click to open
                   </h1>
                 </div>
